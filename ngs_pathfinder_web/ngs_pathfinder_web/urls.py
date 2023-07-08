67087c4a5dda71f1749feedd509d5373207d7d2f
WQYeo <wqyeo@outlook.com>
2023-07-09 01:54:34 +0800

    setup link to url

--- ngs_pathfinder_web/ngs_pathfinder_web/urls.py
+++ ngs_pathfinder_web/ngs_pathfinder_web/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("add-minerals/", views.add_mineral_node, name="add-mineral"),
+    # path("add-minerals/", views.add_mineral_node, name="add-mineral"),
+    path("generate_graph/", views.create_heu_graph, name="gen-graph"),
     path("", views.index, name="index"),
-    path('admin/', admin.site.urls),
-    path("do-pathfind/", views.do_pathfind, name="do-pathfind")    
+    path("admin/", admin.site.urls),
+    path("do-pathfind/", views.do_pathfind, name="do-pathfind"),
 ]
--- ngs_pathfinder_web/ngs_pathfinder_web/views.py
+++ ngs_pathfinder_web/ngs_pathfinder_web/views.py
@@ -7,13 +7,17 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .node import Node
 from .pathfinder import find_shortest_path
+from .setup_graph import generate_heu_graph_data
 
-def _save_mineral_data(name_id: str, mineral_type: str, lat: float, lng: float, region: str) -> None:
-    file_path = os.path.join('static/data/minerals', name_id + ".json")
+
+def _save_mineral_data(
+    name_id: str, mineral_type: str, lat: float, lng: float, region: str
+) -> None:
+    file_path = os.path.join("static/data/minerals", name_id + ".json")
 
     data = []
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
     except FileNotFoundError:
         data = []
@@ -21,35 +25,37 @@ def _save_mineral_data(name_id: str, mineral_type: str, lat: float, lng: float, 
 
     id = name_id + str(len(data))
     insertion = {
-        'id': id,
-        'type': mineral_type,
-        'lat': lat,
-        'lng': lng,
-        'region': region
+        "id": id,
+        "type": mineral_type,
+        "lat": lat,
+        "lng": lng,
+        "region": region,
     }
     data.append(insertion)
 
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
 
+
 def index(request):
-    return render(request, 'main.html')
+    return render(request, "main.html")
+
 
 def add_mineral_node(request):
-
-    name_id = request.GET.get('nameId')
-    mineral_type = request.GET.get('type')
-    lat = request.GET.get('lat')
-    lng = request.GET.get('lng')
-    region = request.GET.get('region')
+    name_id = request.GET.get("nameId")
+    mineral_type = request.GET.get("type")
+    lat = request.GET.get("lat")
+    lng = request.GET.get("lng")
+    region = request.GET.get("region")
 
     _save_mineral_data(name_id, mineral_type, lat, lng, region)
 
-    response_data = {'result': 'OK'}
+    response_data = {"result": "OK"}
     return JsonResponse(response_data)
 
+
 def do_pathfind(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.body)
         print(data)
         if (len(data)) <= 1:
@@ -57,7 +63,9 @@ def do_pathfind(request):
 
         nodes = []
         for key, value in data.items():
-            new_node = Node(key, value['lat'], value['lng'], value['category'], value['region'])
+            new_node = Node(
+                key, value["lat"], value["lng"], value["category"], value["region"]
+            )
             nodes.append(new_node)
 
         # Do a simple, dummy pathfinding for now.
@@ -66,13 +74,17 @@ def do_pathfind(request):
         result = []
         for node in shortest_path:
             node_dict = {
-                'id': node.get_id(),
-                'lat': node.get_lat(),
-                'lng': node.get_lng(),
-                'category': node.get_category(),
-                'region': node.get_region()
+                "id": node.get_id(),
+                "lat": node.get_lat(),
+                "lng": node.get_lng(),
+                "category": node.get_category(),
+                "region": node.get_region(),
             }
             result.append(node_dict)
-        return JsonResponse({'paths': result})
+        return JsonResponse({"paths": result})
+    return JsonResponse({"paths": None})
 
-    return JsonResponse({'paths': None})
+
+def create_heu_graph(request):
+    generate_heu_graph_data()
+    return JsonResponse({"status": "OK"})
